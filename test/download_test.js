a592a7c202492174077d3e075c8e44ae5da586ff
thimo visser <thimo.visser@hotmail.nl>
2016-12-06 15:00:11 +0800

    All tests passing again after refactor

--- test/download_test.js
+++ test/download_test.js
@@ -2,21 +2,22 @@ var assert = require('assert');
 var download = require('../src/download');
 
 describe('Download', function() {
+        var downloadPath = '\\test\\downloads'
         it('downloads sample http file', function(done) {
-            download.start('http://www.brainjar.com/java/host/test.html',process.cwd() + DOWNLOAD_DIR + '/test.html', LOG_OUTPUT, function(succes){
+            download.start('http://www.brainjar.com/java/host/test.html',process.cwd() + downloadPath + '/test.html', false, function(succes){
                 assert(succes,"failed to download bing.com, please check internet connectivity");
                 done();
             });
         });
         it('doesn\'t work when there is no uri', function() {
             assert.throws(function(){ 
-                download.start(null,process.cwd() + DOWNLOAD_DIR + '/bing.html', LOG_OUTPUT, function(succes){
+                download.start(null,process.cwd() + downloadPath + '/bing.html', false, function(succes){
                     assert(false,"failed to download bing.com");
                     done();
                 });
             }, Error,'download allowed a download without uri');
             assert.throws(function(){ 
-                download.start('',process.cwd() + DOWNLOAD_DIR + '/bing.html', LOG_OUTPUT, function(succes){
+                download.start('',process.cwd() + downloadPath + '/bing.html', false, function(succes){
                     assert(false,"failed to download bing.com");
                     done();
                 });
@@ -24,13 +25,13 @@ describe('Download', function() {
         });
         it('doesn\'t work when there is no file path', function() {
             assert.throws(function(){ 
-                download.start('https://bing.com/',null, LOG_OUTPUT, function(succes){
+                download.start('https://bing.com/',null, false, function(succes){
                     assert(false,"failed to download bing.com");
                     done();
                 });
             }, Error,'download allowed a download without uri');
             assert.throws(function(){ 
-                download.start('https://bing.com/','', LOG_OUTPUT, function(succes){
+                download.start('https://bing.com/','', false, function(succes){
                     assert(false,"failed to download bing.com");
                     done();
                 });
