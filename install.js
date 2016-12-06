10cf8d9d5a00a7a6f4b3bfd64b5c96cf1a54a4da
thimo visser <thimo.visser@hotmail.nl>
2016-12-06 17:36:29 +0800

    now prompts user for settings.json

--- install.js
+++ install.js
@@ -1,3 +1,4 @@
+var prompt = require('prompt');
 var fs = require('fs');
 
 var SETTINGS_FILE_PATH = process.cwd() + '\\settings.json'
@@ -18,17 +19,69 @@ var generateJson = function(ciToken,vcs,team,projName,port,downloadPath){
 	"\"\n}"
 }
 
+var promptUser = function(callback){
+
+	var properties = [
+	    {
+		    name: 'token',
+	    	description: 'Please enter your CircleCI token.',
+	    	type: 'string',
+        	required: true
+	    },
+	    {
+		    name: 'vcs',
+	    	description: 'Version control system, can either be github or bitbucket.',
+			validator: /github|bitbucket/,
+			warning: "can only be \"github\" or \"bitbucket\"",
+        	required: true
+	    },
+	    {
+		    name: 'team',
+	    	description: 'Team name of the circle ci project.',
+	    	type: 'string',
+        	required: true
+	    },
+	    {
+		    name: 'projName',
+	    	description: 'Project name of the circle ci project.',
+	    	type: 'string',
+        	required: true
+	    },
+	    {
+		    name: 'port',
+	    	description: 'What port to launch the webhook listener on, default is 3000.',
+	    	type: 'number', 
+        	required: true,
+        	default: 3000
+	    },
+	    {
+		    name: 'downloadPath',
+	    	description: 'Where to store downloaded artifacts, {cwd} will be replaced with the working directory of the project.',
+	    	type: 'string',
+        	required: true
+	    }
+	];
+	prompt.start();
+	prompt.get(properties, function (err, result) {
+	    callback(err,result)
+	});
+}
+
 fs.stat(SETTINGS_FILE_PATH,function(err,stats){
 	if(!err){
 		console.log('settings.json already exists, not creating a new one.')	
 	} else {
 		if(err.code == 'ENOENT'){
 			console.log('settings.json doesn\'t exist yet, creating a new one.')
-			var settingsJson = generateJson('CI_TOKEN','VCS','TEAM','PROJ',3000,'DOWNLOAD_PATH');
-			fs.writeFile(SETTINGS_FILE_PATH,settingsJson,function(err){
-				if (err){console.log('caught error: ', err)} else {
-					console.log('finished writing settings.json');
-				}
+			promptUser(function(err,res){
+				var settingsJson = generateJson(res.token,res.vcs,res.team,res.projName,res.port,res.downloadPath);
+				fs.writeFile(SETTINGS_FILE_PATH,settingsJson,function(err){
+					if (err){console.log('caught error: ', err)} else {
+						console.log("settings.json:")
+						console.log(settingsJson)
+						console.log('your settings.json has been created');
+					}
+				});
 			});
 		} else {
 			console.log('caught error:',err)
