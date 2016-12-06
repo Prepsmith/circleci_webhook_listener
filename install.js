cd6c401a617e1fd1bda71361091673aa55f606eb
thimo visser <thimo.visser@hotmail.nl>
2016-12-06 17:13:07 +0800

    Now generates settings.json

--- install.js
+++ install.js
@@ -1,7 +1,22 @@
 var fs = require('fs');
 
 var SETTINGS_FILE_PATH = process.cwd() + '\\settings.json'
-var NEW_SETTINGS_JSON = '{\n\t\"circle_ci_token\": \"{your circleCI token}\",\n\t\"circle_ci_url\": \"https://circleci.com/api/v1.1/project/{vcs}/{team}/{projName}/\",\n\t\"port\": 3000,\n\t\"download_path\": \"{cwd}/downloads\" // {cwd} will be automtaically replaced with current_working_directory\n}'
+
+var generateJson = function(ciToken,vcs,team,projName,port,downloadPath){
+	return '{\n\t\"circle_ci_token\": \"' + 
+	ciToken +
+	'\",\n\t\"circle_ci_url\": \"https://circleci.com/api/v1.1/project/' +
+	vcs + 
+	'/' +
+	team +
+	'/' +
+	projName +
+	'/\",\n\t\"port\": ' +
+	port +
+	',\n\t\"download_path\": \"' +
+	downloadPath +
+	"\"\n}"
+}
 
 fs.stat(SETTINGS_FILE_PATH,function(err,stats){
 	if(!err){
@@ -9,8 +24,8 @@ fs.stat(SETTINGS_FILE_PATH,function(err,stats){
 	} else {
 		if(err.code == 'ENOENT'){
 			console.log('settings.json doesn\'t exist yet, creating a new one.')
-			console.log("@",SETTINGS_FILE_PATH)
-			fs.writeFile(SETTINGS_FILE_PATH,NEW_SETTINGS_JSON,function(err){
+			var settingsJson = generateJson('CI_TOKEN','VCS','TEAM','PROJ',3000,'DOWNLOAD_PATH');
+			fs.writeFile(SETTINGS_FILE_PATH,settingsJson,function(err){
 				if (err){console.log('caught error: ', err)} else {
 					console.log('finished writing settings.json');
 				}
