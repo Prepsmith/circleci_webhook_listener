853a733d595ae643bde380042b9778e8dd4fbb3a
thimo visser <thimo.visser@hotmail.nl>
2016-12-07 11:22:36 +0800

    settingsLoader now normalizes settings path

--- src/settingsLoader.js
+++ src/settingsLoader.js
@@ -21,7 +21,8 @@ exports.load = function(settingsPath, callback){
             console.log('download_path field was not found in settings.json, using\n' + DEFAULT_DOWNLOAD_PATH + '\nas default') 
             res.download_path = DEFAULT_DOWNLOAD_PATH
         } else {
-            res.download_path = res.download_path.replace(/{cwd}/g, path.normalize(process.cwd()))
+            var replacedPath = res.download_path.replace(/{cwd}/g, path.normalize(process.cwd()))
+            res.download_path = path.normalize(replacedPath);
         }
         console.log('successfully loaded settings');
         callback(null,res);
